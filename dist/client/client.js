253b39f300f5fe4bce85d3d9488247c393cd2064
Sean Bradley <seanwasere@gmail.com>
2020-06-12 20:19:44 +0100

    updates

--- dist/client/client.js
+++ dist/client/client.js
@@ -2,24 +2,151 @@ import * as THREE from '/build/three.module.js';
 import { OrbitControls } from '/jsm/controls/OrbitControls';
 import Stats from '/jsm/libs/stats.module';
 import { GUI } from '/jsm/libs/dat.gui.module';
-import { TWEEN } from '/jsm/libs/tween.module.min';
+import '/cannon/cannon.min';
+import CannonDebugRenderer from './utils/cannonDebugRenderer.js';
+import CannonUtils from './utils/cannonUtils.js';
+import { OBJLoader } from '/jsm/loaders/OBJLoader';
 const scene = new THREE.Scene();
+const axesHelper = new THREE.AxesHelper(5);
+scene.add(axesHelper);
+var light1 = new THREE.SpotLight();
+light1.position.set(2.5, 5, 5);
+light1.angle = Math.PI / 4;
+light1.penumbra = 0.5;
+light1.castShadow = true;
+light1.shadow.mapSize.width = 1024;
+light1.shadow.mapSize.height = 1024;
+light1.shadow.camera.near = 0.5;
+light1.shadow.camera.far = 20;
+scene.add(light1);
+var light2 = new THREE.SpotLight();
+light2.position.set(-2.5, 5, 5);
+light2.angle = Math.PI / 4;
+light2.penumbra = 0.5;
+light2.castShadow = true;
+light2.shadow.mapSize.width = 1024;
+light2.shadow.mapSize.height = 1024;
+light2.shadow.camera.near = 0.5;
+light2.shadow.camera.far = 20;
+scene.add(light2);
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.shadowMap.enabled = true;
+renderer.shadowMap.type = THREE.PCFSoftShadowMap;
 document.body.appendChild(renderer.domElement);
 const controls = new OrbitControls(camera, renderer.domElement);
-const geometry = new THREE.BoxGeometry();
-const material = new THREE.MeshBasicMaterial({ color: 0x00ff00, wireframe: true });
-let myObject3D = new THREE.Object3D();
-myObject3D.position.x = (Math.random() * 4) - 2;
-myObject3D.position.z = (Math.random() * 4) - 2;
-var size = 10;
-var divisions = 10;
-var gridHelper = new THREE.GridHelper(size, divisions);
-gridHelper.position.y = -.5;
-scene.add(gridHelper);
+controls.screenSpacePanning = true;
+const world = new CANNON.World();
+world.gravity.set(0, -9.82, 0);
+//world.broadphase = new CANNON.NaiveBroadphase() //
+//world.solver.iterations = 10
+//world.allowSleep = true
+const normalMaterial = new THREE.MeshNormalMaterial();
+const phongMaterial = new THREE.MeshPhongMaterial();
+const cubeGeometry = new THREE.BoxGeometry(1, 1, 1);
+const cubeMesh = new THREE.Mesh(cubeGeometry, normalMaterial);
+cubeMesh.position.x = -4;
+cubeMesh.position.y = 3;
+cubeMesh.castShadow = true;
+scene.add(cubeMesh);
+const cubeShape = new CANNON.Box(new CANNON.Vec3(.5, .5, .5));
+const cubeBody = new CANNON.Body({ mass: 1 });
+cubeBody.addShape(cubeShape);
+cubeBody.position.x = cubeMesh.position.x;
+cubeBody.position.y = cubeMesh.position.y;
+cubeBody.position.z = cubeMesh.position.z;
+world.addBody(cubeBody);
+const sphereGeometry = new THREE.SphereGeometry();
+const sphereMesh = new THREE.Mesh(sphereGeometry, normalMaterial);
+sphereMesh.position.x = -2;
+sphereMesh.position.y = 3;
+sphereMesh.castShadow = true;
+scene.add(sphereMesh);
+const sphereShape = new CANNON.Sphere(1);
+const sphereBody = new CANNON.Body({ mass: 1 });
+sphereBody.addShape(sphereShape);
+sphereBody.position.x = sphereMesh.position.x;
+sphereBody.position.y = sphereMesh.position.y;
+sphereBody.position.z = sphereMesh.position.z;
+world.addBody(sphereBody);
+const cylinderGeometry = new THREE.CylinderGeometry(1, 1, 2, 8);
+const cylinderMesh = new THREE.Mesh(cylinderGeometry, normalMaterial);
+cylinderMesh.position.x = 0;
+cylinderMesh.position.y = 3;
+cylinderMesh.castShadow = true;
+scene.add(cylinderMesh);
+const cylinderShape = new CANNON.Cylinder(1, 1, 2, 8);
+const cylinderBody = new CANNON.Body({ mass: 1 });
+const cylinderQuaternion = new CANNON.Quaternion();
+cylinderQuaternion.setFromAxisAngle(new CANNON.Vec3(1, 0, 0), Math.PI / 2);
+cylinderBody.addShape(cylinderShape, new CANNON.Vec3, cylinderQuaternion);
+cylinderBody.position.x = cylinderMesh.position.x;
+cylinderBody.position.y = cylinderMesh.position.y;
+cylinderBody.position.z = cylinderMesh.position.z;
+world.addBody(cylinderBody);
+const icosahedronGeometry = new THREE.IcosahedronGeometry(1, 0);
+const icosahedronMesh = new THREE.Mesh(icosahedronGeometry, normalMaterial);
+icosahedronMesh.position.x = 2;
+icosahedronMesh.position.y = 3;
+icosahedronMesh.castShadow = true;
+scene.add(icosahedronMesh);
+const icosahedronShape = CannonUtils.CreateConvexPolyhedron(icosahedronMesh.geometry);
+const icosahedronBody = new CANNON.Body({ mass: 1 });
+icosahedronBody.addShape(icosahedronShape);
+icosahedronBody.position.x = icosahedronMesh.position.x;
+icosahedronBody.position.y = icosahedronMesh.position.y;
+icosahedronBody.position.z = icosahedronMesh.position.z;
+world.addBody(icosahedronBody);
+const torusKnotGeometry = new THREE.TorusKnotGeometry();
+const torusKnotMesh = new THREE.Mesh(torusKnotGeometry, normalMaterial);
+torusKnotMesh.position.x = 4;
+torusKnotMesh.position.y = 3;
+torusKnotMesh.castShadow = true;
+scene.add(torusKnotMesh);
+const torusKnotShape = CannonUtils.CreateTrimesh(torusKnotMesh.geometry);
+const torusKnotBody = new CANNON.Body({ mass: 1 });
+torusKnotBody.addShape(torusKnotShape);
+torusKnotBody.position.x = torusKnotMesh.position.x;
+torusKnotBody.position.y = torusKnotMesh.position.y;
+torusKnotBody.position.z = torusKnotMesh.position.z;
+world.addBody(torusKnotBody);
+let monkeyMesh;
+let monkeyBody;
+let monkeyLoaded = false;
+const objLoader = new OBJLoader();
+objLoader.load('models/monkey.obj', (object) => {
+    scene.add(object);
+    monkeyMesh = object.children[0];
+    monkeyMesh.material = normalMaterial;
+    monkeyMesh.position.x = -2;
+    monkeyMesh.position.y = 20;
+    const monkeyShape = CannonUtils.CreateTrimesh(monkeyMesh.geometry);
+    monkeyBody = new CANNON.Body({ mass: 1 });
+    monkeyBody.addShape(monkeyShape);
+    monkeyBody.position.x = monkeyMesh.position.x;
+    monkeyBody.position.y = monkeyMesh.position.y;
+    monkeyBody.position.z = monkeyMesh.position.z;
+    world.addBody(monkeyBody);
+    monkeyLoaded = true;
+}, (xhr) => {
+    console.log((xhr.loaded / xhr.total * 100) + '% loaded');
+}, (error) => {
+    console.log('An error happened');
+});
+const planeGeometry = new THREE.PlaneGeometry(25, 25);
+const planeMesh = new THREE.Mesh(planeGeometry, phongMaterial);
+planeMesh.rotateX(-Math.PI / 2);
+planeMesh.receiveShadow = true;
+scene.add(planeMesh);
+const planeShape = new CANNON.Plane();
+const planeBody = new CANNON.Body({ mass: 0 });
+planeBody.addShape(planeShape);
+planeBody.quaternion.setFromAxisAngle(new CANNON.Vec3(1, 0, 0), -Math.PI / 2);
+world.addBody(planeBody);
+camera.position.y = 4;
 camera.position.z = 4;
+controls.target.y = 2;
 window.addEventListener('resize', onWindowResize, false);
 function onWindowResize() {
     camera.aspect = window.innerWidth / window.innerHeight;
@@ -27,88 +154,43 @@ function onWindowResize() {
     renderer.setSize(window.innerWidth, window.innerHeight);
     render();
 }
-let myId = "";
-let timestamp = 0;
-const peerCubes = {};
-const socket = io();
-socket.on("connect", function () {
-    console.log("connect");
-});
-socket.on("disconnect", function (message) {
-    console.log("disconnect " + message);
-});
-socket.on("id", (id) => {
-    myId = id;
-    setInterval(() => {
-        socket.emit("update", { t: Date.now(), p: myObject3D.position, r: myObject3D.rotation });
-    }, 50);
-});
-socket.on("peers", (peers) => {
-    let pingStatsHtml = "Socket Ping Stats<br/><br/>";
-    Object.keys(peers).forEach((p) => {
-        timestamp = Date.now();
-        pingStatsHtml += p + " " + (timestamp - peers[p].t) + "ms<br/>";
-        if (!peerCubes[p]) {
-            peerCubes[p] = new THREE.Mesh(geometry, material);
-            peerCubes[p].name = p;
-            scene.add(peerCubes[p]);
-        }
-        else {
-            if (peers[p].p) {
-                new TWEEN.Tween(peerCubes[p].position)
-                    .to({
-                    x: peers[p].p.x,
-                    y: peers[p].p.y,
-                    z: peers[p].p.z
-                }, 50)
-                    .start();
-            }
-            if (peers[p].r) {
-                new TWEEN.Tween(peerCubes[p].rotation)
-                    .to({
-                    x: peers[p].r._x,
-                    y: peers[p].r._y,
-                    z: peers[p].r._z
-                }, 50)
-                    .start();
-            }
-        }
-    });
-    Object.keys(peerCubes).forEach((p) => {
-        if (!peers[p]) {
-            scene.remove(scene.getObjectByName(p));
-        }
-    });
-    document.getElementById("pingStats").innerHTML = pingStatsHtml;
-});
-socket.on("removePeer", (id) => {
-    scene.remove(scene.getObjectByName(id));
-});
 const stats = Stats();
 document.body.appendChild(stats.dom);
 const gui = new GUI();
-const cubeFolder = gui.addFolder("Cube");
-const cubePositionFolder = cubeFolder.addFolder("Position");
-cubePositionFolder.add(myObject3D.position, "x", -5, 5);
-cubePositionFolder.add(myObject3D.position, "z", -5, 5);
-cubePositionFolder.open();
-const cubeRotationFolder = cubeFolder.addFolder("Rotation");
-cubeRotationFolder.add(myObject3D.rotation, "x", 0, Math.PI * 2, 0.01);
-cubeRotationFolder.add(myObject3D.rotation, "y", 0, Math.PI * 2, 0.01);
-cubeRotationFolder.add(myObject3D.rotation, "z", 0, Math.PI * 2, 0.01);
-cubeRotationFolder.open();
-cubeFolder.open();
-const animate = function () {
+const physicsFolder = gui.addFolder("Physics");
+physicsFolder.add(world.gravity, "x", -10.0, 10.0, 0.1);
+physicsFolder.add(world.gravity, "y", -10.0, 10.0, 0.1);
+physicsFolder.add(world.gravity, "z", -10.0, 10.0, 0.1);
+physicsFolder.open();
+const clock = new THREE.Clock();
+const cannonDebugRenderer = new CannonDebugRenderer(scene, world);
+var animate = function () {
     requestAnimationFrame(animate);
     controls.update();
-    TWEEN.update();
-    if (peerCubes[myId]) {
-        camera.lookAt(peerCubes[myId].position);
+    let delta = clock.getDelta();
+    if (delta > .1)
+        delta = .1;
+    world.step(delta);
+    cannonDebugRenderer.update();
+    // Copy coordinates from Cannon.js to Three.js
+    cubeMesh.position.set(cubeBody.position.x, cubeBody.position.y, cubeBody.position.z);
+    cubeMesh.quaternion.set(cubeBody.quaternion.x, cubeBody.quaternion.y, cubeBody.quaternion.z, cubeBody.quaternion.w);
+    sphereMesh.position.set(sphereBody.position.x, sphereBody.position.y, sphereBody.position.z);
+    sphereMesh.quaternion.set(sphereBody.quaternion.x, sphereBody.quaternion.y, sphereBody.quaternion.z, sphereBody.quaternion.w);
+    cylinderMesh.position.set(cylinderBody.position.x, cylinderBody.position.y, cylinderBody.position.z);
+    cylinderMesh.quaternion.set(cylinderBody.quaternion.x, cylinderBody.quaternion.y, cylinderBody.quaternion.z, cylinderBody.quaternion.w);
+    icosahedronMesh.position.set(icosahedronBody.position.x, icosahedronBody.position.y, icosahedronBody.position.z);
+    icosahedronMesh.quaternion.set(icosahedronBody.quaternion.x, icosahedronBody.quaternion.y, icosahedronBody.quaternion.z, icosahedronBody.quaternion.w);
+    torusKnotMesh.position.set(torusKnotBody.position.x, torusKnotBody.position.y, torusKnotBody.position.z);
+    torusKnotMesh.quaternion.set(torusKnotBody.quaternion.x, torusKnotBody.quaternion.y, torusKnotBody.quaternion.z, torusKnotBody.quaternion.w);
+    if (monkeyLoaded) {
+        monkeyMesh.position.set(monkeyBody.position.x, monkeyBody.position.y, monkeyBody.position.z);
+        monkeyMesh.quaternion.set(monkeyBody.quaternion.x, monkeyBody.quaternion.y, monkeyBody.quaternion.z, monkeyBody.quaternion.w);
     }
     render();
     stats.update();
 };
-const render = function () {
+function render() {
     renderer.render(scene, camera);
-};
+}
 animate();
