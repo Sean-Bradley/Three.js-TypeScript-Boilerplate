08745f21c7fa8eeecbb83ba9b87ee20e0252c441
Sean Bradley <seanwasere@gmail.com>
2021-02-07 15:27:46 +0000

    updates for CreateConvexPolyhedron

--- src/client/utils/cannonUtils.ts
+++ src/client/utils/cannonUtils.ts
@@ -6,26 +6,82 @@ class CannonUtils {
 
     public static CreateTrimesh(geometry: THREE.BufferGeometry): CANNON.Trimesh {
         const vertices: number[] = <number[]>geometry.attributes.position.array
-        const indices: number[] = Object.keys(vertices).map(Number)
-        return new CANNON.Trimesh(vertices, indices)
+        const indices: number[] = Object.keys(vertices).map(Number);
+        return new CANNON.Trimesh(vertices, indices);
     }
 
-
     public static CreateConvexPolyhedron(geometry: THREE.BufferGeometry): CANNON.ConvexPolyhedron {
-        const position = geometry.attributes.position.array
+        const position = geometry.attributes.position;
+        const normal = geometry.attributes.normal;
+        let vertices: THREE.Vector3[] = []
+        for (let i = 0; i < position.count; i++) {
+            vertices.push(new THREE.Vector3().fromBufferAttribute(position, i));
+        }
+        const faces: any[] = []
+        for (let i = 0; i < position.count; i += 3) {
+            const vertexNormals = (normal === undefined) ? [] : [
+                new THREE.Vector3().fromBufferAttribute(normal, i),
+                new THREE.Vector3().fromBufferAttribute(normal, i + 1),
+                new THREE.Vector3().fromBufferAttribute(normal, i + 2)
+            ];
+            const face = new THREE.Face3(i, i + 1, i + 2, vertexNormals, []);
+            faces.push(face);
+        }
+
+        const verticesMap: { [key: string]: number } = {}
         const points: CANNON.Vec3[] = []
-        for (let i = 0; i < position.length; i += 3) {
-            const x = position[i]
-            const y = position[i + 1]
-            const z = position[i + 2]
-            points.push(new CANNON.Vec3(x, y, z));
+        const changes: number[] = [];
+        for (let i = 0, il = vertices.length; i < il; i++) {
+            const v = vertices[i];
+            const key = Math.round(v.x * 100) + '_' + Math.round(v.y * 100) + '_' + Math.round(v.z * 100);
+            if (verticesMap[key] === undefined) {
+                verticesMap[key] = i;
+                points.push(new CANNON.Vec3(vertices[i].x, vertices[i].y, vertices[i].z));
+                changes[i] = points.length - 1;
+            } else {
+                changes[i] = changes[verticesMap[key]];
+            }
         }
-        const faces: number[][] = []
-        for (let i = 0; i < position.length / 3; i += 3) {
-            faces.push([i, i + 1, i + 2])
+
+        const faceIdsToRemove = [];
+        for (let i = 0, il = faces.length; i < il; i++) {
+            const face = faces[i];
+            face.a = changes[face.a];
+            face.b = changes[face.b];
+            face.c = changes[face.c];
+            const indices = [face.a, face.b, face.c];
+            for (let n = 0; n < 3; n++) {
+                if (indices[n] === indices[(n + 1) % 3]) {
+                    faceIdsToRemove.push(i);
+                    break;
+                }
+            }
         }
-        return new CANNON.ConvexPolyhedron(points, faces);
+
+        for (let i = faceIdsToRemove.length - 1; i >= 0; i--) {
+            const idx = faceIdsToRemove[i];
+            faces.splice(idx, 1);
+        }
+
+        const cannonFaces: number[][] = faces.map(function (f) {
+            return [f.a, f.b, f.c]
+        })
+
+        return new CANNON.ConvexPolyhedron(points, cannonFaces);
+    }
+
+    public static offsetCenterOfMass(body: CANNON.Body, centreOfMass: CANNON.Vec3) {
+        body.shapeOffsets.forEach(function (offset) {
+            centreOfMass.vadd(offset, centreOfMass)
+        })
+        centreOfMass.scale(1 / body.shapes.length, centreOfMass)
+        body.shapeOffsets.forEach(function (offset) {
+            offset.vsub(centreOfMass, offset)
+        })
+        const worldCenterOfMass = new CANNON.Vec3()
+        body.vectorToWorldFrame(centreOfMass, worldCenterOfMass)
+        body.position.vadd(worldCenterOfMass, body.position)
     }
 }
 
-export default CannonUtils
+export default CannonUtils;
