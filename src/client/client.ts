48eb95efd70087d793413014e2cc04d6119e4a80
Sean Bradley <seanwasere@gmail.com>
2021-07-17 17:59:03 +0100

    three r130

--- src/client/client.ts
+++ src/client/client.ts
@@ -4,26 +4,31 @@ import Stats from 'three/examples/jsm/libs/stats.module'
 import { GUI } from 'three/examples/jsm/libs/dat.gui.module'
 import { TWEEN } from 'three/examples/jsm/libs/tween.module.min'
 
-const scene: THREE.Scene = new THREE.Scene()
+declare let io: any
 
-const camera: THREE.PerspectiveCamera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
+const scene = new THREE.Scene()
 
-const renderer: THREE.WebGLRenderer = new THREE.WebGLRenderer()
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
+
+const renderer = new THREE.WebGLRenderer()
 renderer.setSize(window.innerWidth, window.innerHeight)
 document.body.appendChild(renderer.domElement)
 
 const controls = new OrbitControls(camera, renderer.domElement)
 
-const geometry: THREE.BoxGeometry = new THREE.BoxGeometry()
-const material: THREE.MeshBasicMaterial = new THREE.MeshBasicMaterial({ color: 0x00ff00, wireframe: true })
+const geometry = new THREE.BoxGeometry()
+const material = new THREE.MeshBasicMaterial({
+    color: 0x00ff00,
+    wireframe: true,
+})
 
-const myObject3D: THREE.Object3D = new THREE.Object3D()
-myObject3D.position.x = (Math.random() * 4) - 2
-myObject3D.position.z = (Math.random() * 4) - 2
+const myObject3D = new THREE.Object3D()
+myObject3D.position.x = Math.random() * 4 - 2
+myObject3D.position.z = Math.random() * 4 - 2
 
-const gridHelper = new THREE.GridHelper(10, 10);
-gridHelper.position.y = -.5
-scene.add(gridHelper);
+const gridHelper = new THREE.GridHelper(10, 10)
+gridHelper.position.y = -0.5
+scene.add(gridHelper)
 
 camera.position.z = 4
 
@@ -35,27 +40,31 @@ function onWindowResize() {
     render()
 }
 
-let myId: string = ""
+let myId = ''
 let timestamp = 0
 const clientCubes: { [id: string]: THREE.Mesh } = {}
 const socket: SocketIOClient.Socket = io()
-socket.on("connect", function () {
-    console.log("connect")
+socket.on('connect', function () {
+    console.log('connect')
 })
-socket.on("disconnect", function (message: any) {
-    console.log("disconnect " + message)
+socket.on('disconnect', function (message: any) {
+    console.log('disconnect ' + message)
 })
-socket.on("id", (id: any) => {
+socket.on('id', (id: any) => {
     myId = id
     setInterval(() => {
-        socket.emit("update", { t: Date.now(), p: myObject3D.position, r: myObject3D.rotation })
+        socket.emit('update', {
+            t: Date.now(),
+            p: myObject3D.position,
+            r: myObject3D.rotation,
+        })
     }, 50)
 })
-socket.on("clients", (clients: any) => {
-    let pingStatsHtml = "Socket Ping Stats<br/><br/>"
+socket.on('clients', (clients: any) => {
+    let pingStatsHtml = 'Socket Ping Stats<br/><br/>'
     Object.keys(clients).forEach((p) => {
         timestamp = Date.now()
-        pingStatsHtml += p + " " + (timestamp - clients[p].t) + "ms<br/>"
+        pingStatsHtml += p + ' ' + (timestamp - clients[p].t) + 'ms<br/>'
         if (!clientCubes[p]) {
             clientCubes[p] = new THREE.Mesh(geometry, material)
             clientCubes[p].name = p
@@ -63,45 +72,49 @@ socket.on("clients", (clients: any) => {
         } else {
             if (clients[p].p) {
                 new TWEEN.Tween(clientCubes[p].position)
-                    .to({
-                        x: clients[p].p.x,
-                        y: clients[p].p.y,
-                        z: clients[p].p.z
-                    }, 50)
+                    .to(
+                        {
+                            x: clients[p].p.x,
+                            y: clients[p].p.y,
+                            z: clients[p].p.z,
+                        },
+                        50
+                    )
                     .start()
             }
             if (clients[p].r) {
                 new TWEEN.Tween(clientCubes[p].rotation)
-                    .to({
-                        x: clients[p].r._x,
-                        y: clients[p].r._y,
-                        z: clients[p].r._z
-                    }, 50)
+                    .to(
+                        {
+                            x: clients[p].r._x,
+                            y: clients[p].r._y,
+                            z: clients[p].r._z,
+                        },
+                        50
+                    )
                     .start()
             }
         }
-    });
-    (document.getElementById("pingStats") as HTMLDivElement).innerHTML = pingStatsHtml
+    })
+    ;(document.getElementById('pingStats') as HTMLDivElement).innerHTML = pingStatsHtml
 })
-socket.on("removeClient", (id: string) => {
-    scene.remove(scene.getObjectByName(id) as THREE.Object3D);
+socket.on('removeClient', (id: string) => {
+    scene.remove(scene.getObjectByName(id) as THREE.Object3D)
 })
-
-
 
 const stats = Stats()
 document.body.appendChild(stats.dom)
 
 const gui = new GUI()
-const cubeFolder = gui.addFolder("Cube")
-const cubePositionFolder = cubeFolder.addFolder("Position")
-cubePositionFolder.add(myObject3D.position, "x", -5, 5)
-cubePositionFolder.add(myObject3D.position, "z", -5, 5)
+const cubeFolder = gui.addFolder('Cube')
+const cubePositionFolder = cubeFolder.addFolder('Position')
+cubePositionFolder.add(myObject3D.position, 'x', -5, 5)
+cubePositionFolder.add(myObject3D.position, 'z', -5, 5)
 cubePositionFolder.open()
-const cubeRotationFolder = cubeFolder.addFolder("Rotation")
-cubeRotationFolder.add(myObject3D.rotation, "x", 0, Math.PI * 2, 0.01)
-cubeRotationFolder.add(myObject3D.rotation, "y", 0, Math.PI * 2, 0.01)
-cubeRotationFolder.add(myObject3D.rotation, "z", 0, Math.PI * 2, 0.01)
+const cubeRotationFolder = cubeFolder.addFolder('Rotation')
+cubeRotationFolder.add(myObject3D.rotation, 'x', 0, Math.PI * 2, 0.01)
+cubeRotationFolder.add(myObject3D.rotation, 'y', 0, Math.PI * 2, 0.01)
+cubeRotationFolder.add(myObject3D.rotation, 'z', 0, Math.PI * 2, 0.01)
 cubeRotationFolder.open()
 cubeFolder.open()
 
@@ -110,7 +123,7 @@ const animate = function () {
 
     controls.update()
 
-    TWEEN.update();
+    TWEEN.update()
 
     if (clientCubes[myId]) {
         camera.lookAt(clientCubes[myId].position)
@@ -119,9 +132,10 @@ const animate = function () {
     render()
 
     stats.update()
-};
+}
 
 const render = function () {
     renderer.render(scene, camera)
 }
-animate();
+
+animate()
