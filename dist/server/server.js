6cd284099112ae621a7b26111d06c7537d4d54b2
Sean Bradley <seanwasere@gmail.com>
2021-07-18 22:44:44 +0100

    three r130

---
+++ dist/server/server.js
@@ -0,0 +1,38 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const path_1 = __importDefault(require("path"));
+const http_1 = __importDefault(require("http"));
+const port = 3000;
+class App {
+    constructor(port) {
+        this.port = port;
+        const app = express_1.default();
+        app.use(express_1.default.static(path_1.default.join(__dirname, '../client')));
+        // In the webpack version of the boilerplate, it is not necessary
+        // to add static references to the libs in node_modules if
+        // you are using module specifiers in your client.ts imports.
+        //
+        // Visit https://sbcode.net/threejs/module-specifiers/ for info about module specifiers
+        //
+        // This server.ts is only useful if you are running this on a production server or you
+        // want to see how the production version of bundle.js works
+        //
+        // to use this server.ts
+        // # npm run build        (this creates the production version of bundle.js and places it in ./dist/client/)
+        // # tsc -p ./src/server  (this compiles ./src/server/server.ts into ./dist/server/server.js)
+        // # npm start            (this starts nodejs with express and serves the ./dist/client folder)
+        //
+        // visit http://127.0.0.1:3000
+        this.server = new http_1.default.Server(app);
+    }
+    Start() {
+        this.server.listen(this.port, () => {
+            console.log(`Server listening on port ${this.port}.`);
+        });
+    }
+}
+new App(port).Start();
