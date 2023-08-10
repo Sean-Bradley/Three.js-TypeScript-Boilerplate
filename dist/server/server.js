c16b24375f643228a55b4cb9808ef5046cedaadc
Sean Bradley <seanwasere@gmail.com>
2023-08-10 15:03:02 +0100

    updates

--- dist/server/server.js
+++ dist/server/server.js
@@ -10,14 +10,23 @@ const port = 3000;
 class App {
     constructor(port) {
         this.port = port;
-        const app = express_1.default();
+        const app = (0, express_1.default)();
         app.use(express_1.default.static(path_1.default.join(__dirname, '../client')));
-        app.use('/build/three.module.js', express_1.default.static(path_1.default.join(__dirname, '../../node_modules/three/build/three.module.js')));
-        app.use('/jsm/webxr/VRButton', express_1.default.static(path_1.default.join(__dirname, '../../node_modules/three/examples/jsm/webxr/VRButton.js')));
-        app.use('/statsvr', express_1.default.static(path_1.default.join(__dirname, '../../node_modules/statsvr/dist/client/statsvr.js')));
-        app.use('/teleportvr', express_1.default.static(path_1.default.join(__dirname, '../../node_modules/teleportvr/dist/client/teleportvr.js')));
-        app.use('/grabvr', express_1.default.static(path_1.default.join(__dirname, '../../node_modules/grabvr/dist/client/grabvr.js')));
-        app.use('/cannon/cannon.min', express_1.default.static(path_1.default.join(__dirname, '../../node_modules/cannon/build/cannon.min.js')));
+        // In the webpack version of the boilerplate, it is not necessary 
+        // to add static references to the libs in node_modules if 
+        // you are using module specifiers in your client.ts imports. 
+        //
+        // Visit https://sbcode.net/threejs/module-specifiers/ for info about module specifiers
+        //
+        // This server.ts is only useful if you are running this on a production server or you
+        // want to see how the production version of bundle.js works
+        // 
+        // to use this server.ts
+        // # npm run build        (this creates the production version of bundle.js and places it in ./dist/client/)
+        // # tsc -p ./src/server  (this compiles ./src/server/server.ts into ./dist/server/server.js)
+        // # npm start            (this starts nodejs with express and serves the ./dist/client folder)
+        // 
+        // visit http://127.0.0.1:3000
         this.server = new http_1.default.Server(app);
     }
     Start() {
@@ -27,4 +36,3 @@ class App {
     }
 }
 new App(port).Start();
-//# sourceMappingURL=server.js.map
