a7cff899d5533d87aef58a926d14f7993ca073a7
Sean Bradley <seanwasere@gmail.com>
2023-08-10 14:50:43 +0100

    updates

--- dist/server/server.js
+++ dist/server/server.js
@@ -10,12 +10,23 @@ const port = 3000;
 class App {
     constructor(port) {
         this.port = port;
-        const app = express_1.default();
+        const app = (0, express_1.default)();
         app.use(express_1.default.static(path_1.default.join(__dirname, '../client')));
-        app.use('/build/three.module.js', express_1.default.static(path_1.default.join(__dirname, '../../node_modules/three/build/three.module.js')));
-        app.use('/jsm/controls/OrbitControls', express_1.default.static(path_1.default.join(__dirname, '../../node_modules/three/examples/jsm/controls/OrbitControls.js')));
-        app.use('/jsm/libs/stats.module', express_1.default.static(path_1.default.join(__dirname, '../../node_modules/three/examples/jsm/libs/stats.module.js')));
-        app.use('/jsm/libs/dat.gui.module', express_1.default.static(path_1.default.join(__dirname, '../../node_modules/three/examples/jsm/libs/dat.gui.module.js')));
+        // In the webpack version of the boilerplate, it is not necessary 
+        // to add static references to the libs in node_modules if 
+        // you are using module specifiers in your client.ts imports. 
+        //
+        // Visit https://sbcode.net/threejs/module-specifiers/ for info about module specifiers
+        //
+        // This server.ts is only useful if you are running this on a production server or you
+        // want to see how the production version of bundle.js works
+        // 
+        // to use this server.ts
+        // # npm run build        (this creates the production version of bundle.js and places it in ./dist/client/)
+        // # tsc -p ./src/server  (this compiles ./src/server/server.ts into ./dist/server/server.js)
+        // # npm start            (this starts nodejs with express and serves the ./dist/client folder)
+        // 
+        // visit http://127.0.0.1:3000
         this.server = new http_1.default.Server(app);
     }
     Start() {
