f12182c956c5201f14b9b31ce9dbeb21188cbdff
Sean Bradley <seanwasere@gmail.com>
2024-09-08 22:25:09 +0100

    upates

--- src/client/client.ts
+++ src/client/client.ts
@@ -1,5 +1,5 @@
 import * as THREE from 'three'
-import { OrbitControls } from 'three/addons/controls/OrbitControls.js'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
 
 const scene = new THREE.Scene()
 
