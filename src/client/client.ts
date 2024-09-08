8792f4dfa2259a9e508fc597aa30f5bbc8e9cd0f
Sean Bradley <seanwasere@gmail.com>
2024-09-08 23:00:26 +0100

    updates

--- src/client/client.ts
+++ src/client/client.ts
@@ -1,5 +1,5 @@
 import * as THREE from 'three'
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 
 const scene = new THREE.Scene()
 
