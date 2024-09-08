48f7e96a8241f222f256657e91c64f6d4d8cb7a5
Sean Bradley <seanwasere@gmail.com>
2024-09-08 23:11:21 +0100

    updates

--- src/client/client.ts
+++ src/client/client.ts
@@ -1,15 +1,10 @@
 import * as THREE from 'three'
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
-import Stats from 'three/examples/jsm/libs/stats.module'
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js'
+import Stats from 'three/addons/libs/stats.module.js'
 
 const scene = new THREE.Scene()
 
-const camera = new THREE.PerspectiveCamera(
-    75,
-    window.innerWidth / window.innerHeight,
-    0.1,
-    1000
-)
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
 camera.position.z = 2
 
 const renderer = new THREE.WebGLRenderer()
