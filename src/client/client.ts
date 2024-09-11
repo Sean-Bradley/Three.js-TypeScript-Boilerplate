a6aeba807da262ca1a0f2e0a6720cdb11209f124
sean bradley <seanwasere@gmail.com>
2024-09-11 11:58:31 +0100

    updates

--- src/client/client.ts
+++ src/client/client.ts
@@ -1,6 +1,6 @@
 import * as THREE from 'three'
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
-import Stats from 'three/examples/jsm/libs/stats.module'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+import Stats from 'three/examples/jsm/libs/stats.module.js'
 import { CSG } from './utils/CSGMesh'
 
 const scene = new THREE.Scene()
